74dbe99fabd0eae96923e784dec487afe348cf8e
Allam Ferreira Costa <111925705+4llam@users.noreply.github.com>
2024-04-24 11:06:44 -0300

    inicio

---
+++ app.py
@@ -0,0 +1,9 @@
+print('\n𝓓𝓮𝓵𝓲𝓬𝓲𝓪𝓼 𝓔𝔁𝓹𝓻𝓮𝓼𝓼\n')
+
+print('1. Cadastrar Resturante')
+print('2. Listar Restaurantes')
+print('3. Ativar Restaurante')
+print('3. Sair do app\n')
+
+opcao_selecionada = input('Escolha uma opção: ') 
+print(f'Você escolheu a opção {opcao_selecionada}')
---
+++ exercicios.py
@@ -0,0 +1,19 @@
+#1
+print('Python na Escola de Programação da Alura')
+
+#2
+nome = 'Allam'
+idade = 20
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+#3
+print("""
+      A
+      L
+      U
+      R
+      A""")
+
+#4
+pi_arredondado = 3.14159
+print(f'O valor arredondado de pi é: {pi_arredondado:.2f}')
